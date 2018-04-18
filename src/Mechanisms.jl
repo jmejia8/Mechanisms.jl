1edc6347b362947bd797ab75e2318020f4a36bcc
Jesús Mejía <jesusmejded@gmail.com>
2018-04-18 12:23:18 -0500

    export functions

--- src/Mechanisms.jl
+++ src/Mechanisms.jl
@@ -1,9 +1,10 @@
 module Mechanisms
 
-export getOptimizationProblem
+export getOptimizationProblem, animateFourBarMechanism
 export PTS_VERTICAL_LINE, PTS_ELLIPTIC, PTS_PAIR_1, PTS_PAIR_2
 
 include("testPoints.jl")
 include("fourBarMechanism.jl")
+include("animateFourBarMechanism.jl")
 
 end # module
