870cee392181e0207b7aeba7b3d51c49bc644092
Jesús Mejía <jesusmejded@gmail.com>
2019-10-24 19:29:47 -0500

    no more plots

--- src/Mechanisms.jl
+++ src/Mechanisms.jl
@@ -6,7 +6,6 @@ export PTS_VERTICAL_LINE, PTS_ELLIPTIC, PTS_PAIR_1, PTS_PAIR_2
 include("externals.jl")
 include("testPoints.jl")
 include("fourBarMechanism.jl")
-include("animateFourBarMechanism.jl")
 include("fourBarControl.jl")
 
 end # module
--- src/externals.jl
+++ src/externals.jl
@@ -1,16 +1,7 @@
 if VERSION < v"0.7.0"
-    if Pkg.installed("Plots") == nothing
-        Pkg.add("Plots")
-    end
-
     if Pkg.installed("DifferentialEquations") == nothing
         Pkg.add("DifferentialEquations")
     end
 end
 
 using DifferentialEquations
-using Plots
-
-gr(size=(500, 500),
-   dpi = 19,
-   legend=false)
