734ceeaee813c5cf55fbdd13aee3cd6bb960e73c
Jesús Mejía <jesusmejded@gmail.com>
2019-10-25 02:10:00 -0500

    faster load

--- src/Mechanisms.jl
+++ src/Mechanisms.jl
@@ -3,9 +3,9 @@ module Mechanisms
 export getOptimizationProblem, animateFourBarMechanism,getDynamicOptimizationProblem
 export PTS_VERTICAL_LINE, PTS_ELLIPTIC, PTS_PAIR_1, PTS_PAIR_2
 
-include("externals.jl")
+# include("externals.jl")
+# include("fourBarControl.jl")
 include("testPoints.jl")
 include("fourBarMechanism.jl")
-include("fourBarControl.jl")
 
 end # module
